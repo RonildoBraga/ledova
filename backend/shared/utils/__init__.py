476807621bb2008f457129910a07ab8dbb11be4d
Ronildo Da Rocha Braga Jr <ronildo.braga.junior@gmail.com>
2026-09-10 18:45:23 +1000

    fix(trading): shorten new swap signing windows to fifteen minutes

--- backend/shared/utils/__init__.py
+++ backend/shared/utils/__init__.py
@@ -1,17 +1,10 @@
 from shared.utils.csv_export import csv_cell
 from shared.utils.request import get_client_ip
-from shared.utils.signature import (
-    generate_order_cancel_message,
-    generate_order_create_message,
-    recover_address_from_signature,
-    verify_signature,
-)
+from shared.utils.signature import recover_address_from_signature, verify_signature
 
 __all__ = [
     "csv_cell",
     "get_client_ip",
-    "generate_order_cancel_message",
-    "generate_order_create_message",
     "recover_address_from_signature",
     "verify_signature",
 ]
